76753f051f86e5e65bc5b6a100d6c731b3775fc7
Scinawa <alessandro.luongo@studenti.unimi.it>
2016-08-03 01:56:35 +0200

    pep8

--- SymClock.py
+++ SymClock.py
@@ -1,4 +1,4 @@
-import pygame,math,time
+import pygame, math, time
 import settings
 
 from layout import Layout
@@ -9,39 +9,36 @@ pygame.init()
 size = settings.General['width'], settings.General['height']
 screen = pygame.display.set_mode(size)
 
+
 class Clock:
-	# cambiare la nomenclatura per i nomi lunghi?
+    # cambiare la nomenclatura per i nomi lunghi?
 
-	def __init__(self, hours, decamins, mins):
-		self.hours = hours
-		self.decamins = decamins
-		self.mins = mins
-	def display_time(self):
-		h, m = time.localtime()[3:5]
-		self.hours.change_state(h)
-		self.decamins.change_state(m / 10)
-		self.mins.change_state(m % 10)
-	def test_time(self,h,m):
-		self.hours.change_state(h)
-		self.decamins.change_state(m / 10)
-		self.mins.change_state(m % 10)		
+    def __init__(self, hours, decamins, mins):
+        self.hours = hours
+        self.decamins = decamins
+        self.mins = mins
 
+    def display_time(self):
+        h, m = time.localtime()[3:5]
+        self.hours.change_state(h)
+        self.decamins.change_state(m / 10)
+        self.mins.change_state(m % 10)
 
-
+    def test_time(self, h, m):
+        self.hours.change_state(h)
+        self.decamins.change_state(m / 10)
+        self.mins.change_state(m % 10)
 
 
 if __name__ == "__main__":
-	l=Layout(screen)
-	c = Clock(l.balls, l.triangle, l.penthagon)
-	while True:
-		c.display_time()
-		l.draw()
-		time.sleep(20)
-	
+    l = Layout(screen)
+    c = Clock(l.balls, l.triangle, l.penthagon)
+    while True:
+        c.display_time()
+        l.draw()
+        time.sleep(20)
+
 
 
 
 # non mi piace molto l'organizzazione, meglio da clock aggiungere gli elementi al layout forse
-
-
-
--- settings.py
+++ settings.py
@@ -1,78 +1,75 @@
-
-
 General = {
-	'width'		: 630,
-	'height'	: 600,
-	'background': (8,20,40),
-	'drawOrder'	: ['penthagon','triangle','ball1','ball2','ball3','ball4']
+    'width': 630,
+    'height': 600,
+    'background': (8, 20, 40),
+    'drawOrder': ['penthagon', 'triangle', 'ball1', 'ball2', 'ball3', 'ball4']
 }
 
 
 # Triangle
 Decaminutes = {
-	# the size of the square area with respect to the window width (in percentage)
-	'size'		: 31.75,
+    # the size of the square area with respect to the window width (in percentage)
+    'size': 31.75,
 
-	# where to draw the triangle
-	'position'	:	(35, 45.24),
+    # where to draw the triangle
+    'position': (35, 45.24),
 
-	# an offset initial integer rotation
-	'offset'	:	0,
+    # an offset initial integer rotation
+    'offset': 0,
 
-	# the colors of the vertices
-	1 : (255, 0, 0),
-	2 : (0, 255, 0),
-	3 : (0, 0, 255)
+    # the colors of the vertices
+    1: (255, 0, 0),
+    2: (0, 255, 0),
+    3: (0, 0, 255)
 }
 
 # Penthagon
 Minutes = {
-	# the size of the square area with respect to the window width (in percentage)
-	'size'		: 68.25,
+    # the size of the square area with respect to the window width (in percentage)
+    'size': 68.25,
 
-	# where to draw the penthagon
-	'position'	:	(16.67, 26.98),
+    # where to draw the penthagon
+    'position': (16.67, 26.98),
 
-	# an offset initial integer rotation
-	'offset'	:	0,
-	
-	# the colors of the vertices
-	1 : (204,255,0),
-	2 : (127,66,51),
-	3 : (64,120,211),
-	4 : (238,47,127),
-	5 : (237,2,11)
+    # an offset initial integer rotation
+    'offset': 0,
+
+    # the colors of the vertices
+    1: (204, 255, 0),
+    2: (127, 66, 51),
+    3: (64, 120, 211),
+    4: (238, 47, 127),
+    5: (237, 2, 11)
 }
 
-
 Hours = [
-# sarebbe piu' preciso mettere i colori a parte. Ne vale la pena?
-# size is  in percentage with respect to the window width
-		
-	{ # First ball
-	
-		'size'		: 15.87,
-		'position'	: (16.66, 17.46),
-		'color'		: (0, 0, 32)
-	},
-    { # Second ball	
-	
-		'size'		: 15.87,
-		'position'	: (35, 7.94),
-		'color'		: (0, 0, 64)
-	},
-	{ # Third ball
-	
-		'size'		: 15.87,
-		'position'	: (53.33, 7.94),
-		'color'		: (0, 0, 96)
-	},
-	{ # Fourth ball
-	
-		'size'		: 15.87,
-		'position'	: (71.67, 17.46),
-		'color'		: (0, 0, 128)
-	}
+    # sarebbe piu' preciso mettere i colori a parte. Ne vale la pena?
+    # size is  in percentage with respect to the window width
+
+    {  # First ball
+
+       'size': 15.87,
+       'position': (16.66, 17.46),
+       'color': (0, 0, 32)
+       },
+    {  # Second ball
+
+       'size': 15.87,
+       'position': (35, 7.94),
+       'color': (0, 0, 64)
+       },
+    {  # Third ball
+
+       'size': 15.87,
+       'position': (53.33, 7.94),
+       'color': (0, 0, 96)
+       },
+    {  # Fourth ball
+
+       'size': 15.87,
+       'position': (71.67, 17.46),
+       'color': (0, 0, 128)
+       }
 ]
 
 #	verrebbe piu' elegante un file di configurazione con classi? Penso proprio di si'
