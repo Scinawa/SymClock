a6dfda18a29b34aef6b46cd09617cefae71bc186
Scinawa <alessandro.luongo@studenti.unimi.it>
2016-08-03 12:15:35 +0200

    creato setup.py e creato file di configurazione.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='SymClock',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Nunzio,Scinawa',
+    author_email='',
+    description='SymClock'
+)
